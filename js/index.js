9dfa8c7bf22214eb8e0445e87430d5bf8ae24606
piasun <77275966+piasun@users.noreply.github.com>
2022-10-06 09:31:56 +0200

    update index css

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const scrollProjects = document.querySelector(".scroll-projects");
+const scrollDown = document.querySelector(".scroll-down");
+
+
+scrollDown.addEventListener("click", function () {
+    scrollProjects.scrollTop += 25;
+});
+
+

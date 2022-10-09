fe65739177513033805a16aadf249413d1f3ffae
piasun <77275966+piasun@users.noreply.github.com>
2022-10-09 09:55:39 +0200

    added contact section index html+css+js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,44 @@
-const scrollProjects = document.querySelector(".scroll-projects");
-const scrollDown = document.querySelector(".scroll-down");
+//email js//
 
+function validate() {
+    let name = document.querySelector('.name')
+    let email = document.querySelector('.email')
+    let msg = document.querySelector('.message')
+    let sendBtn = document.querySelector('.send-btn')
 
-scrollDown.addEventListener("click", function () {
-    scrollProjects.scrollTop += 25;
-});
+    sendBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (name.value == "" || email.value == "" || msg.value == ""){
+            emptyerror();
+        } else {
+            sendmail(name.value, email.value, msg.value);
+            success();
+        }
+    });
+}
+validate();
 
+function sendmail(name,email,msg){
+    emailjs.send("service_yjcv0er","template_t27qed9",{
+        from_name: email,
+        to_name: name,
+        message: msg,
+        });
+}
+
+function emptyerror() {
+    swal({
+        title:"Oh no...",
+        text:"Fields can not be empty!",
+        icon: "error",
+    });
+}
+
+function success() {
+    swal({
+        title:"Email sent successfully",
+        text:"I will get back to you as soon as possible",
+        icon: "Success",
+    });
+}
 
